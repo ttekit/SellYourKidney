1c352a1715d2e8fa9399a7ae4a73280188bf9b6a
Tebiog <tekityt@gmail.com>
2022-11-08 08:50:40 +0200

    fixed cart and redesigned it

--- assets/js/cartScript.js
+++ assets/js/cartScript.js
@@ -1,10 +1,11 @@
+
 window.addEventListener("load", function () {
     let $addToCartBtn = $(".addToCartBtn");
     let cartArr = [];
     $addToCartBtn.on("click", (e) => {
         let product = {
             id: $(e.target).parent().find(".product-id").text(),
-            name: $(e.target).parent().find(".name").text(),
+            name: $(e.target).parent().find(".name").text().toUpperCase(),
             price: $(e.target).parent().find(".price").text()
         };
         cartArr.push(product);
@@ -13,23 +14,25 @@ window.addEventListener("load", function () {
     let $cartButton = $(".cart-button");
     let hasPressed = false;
     let $cartContainer = $(".cart-block");
+
     $cartButton.on("click", () => {
+        $elemContainer = $cartContainer.find("ul");
         //Changing visibility of cart div
         if (hasPressed === false) {
             $cartContainer.fadeIn(500);
             hasPressed = true;
-            $elemContainer = $cartContainer.find("ul");
             for(let i = 0; i < cartArr.length; i++){
                 $elemContainer.append(`<li class="row cart-li">
                     <p>${cartArr[i].name}</p>
                     <p>${cartArr[i].price}</p>
-                    <a type="button" class="remove-cart-elem">-</a>
+                    <a type="button" class="remove-cart-elem">del</a>
                 </li>`);
                 console.log(cartArr[i]);
             }
         } else {
             $cartContainer.fadeOut(500);
             hasPressed = false;
+            $elemContainer.empty();
         }
         //Filling this div
     })
